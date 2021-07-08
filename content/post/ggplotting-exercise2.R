cbb93fc457c966e1399fbbdcf9da99853edfb248
Martin Schultze <schultze@psych.uni-frankfurt.de>
2021-07-08 08:26:55 +0200

    Add exercise 2.

---
+++ content/post/ggplotting-exercise2.R
@@ -0,0 +1,17 @@
+#### Workshop ggplotting, Juli 2021
+#### Martin Schultze & Janine Buchholz
+#### Material: https://pandar.netlify.app/extras/#ggplotting
+
+
+## Übung 2
+
+# Lassen Sie folgenden Code laufen, um Ihre Aufgabe zu erhalten.
+
+cat(paste(" Ihre Aufgabe:\n",
+          "Bitte erzeugen Sie ein",
+            sample(c("Balkendiagramm", "Histogramm", "Violin Plot", 
+                     "Boxplot", "Scatterplot", "Liniendiagramm/Zeitreihe"),1),
+            "für den folgenden Zweck:",
+            sample(c("Explorativ", "Wissenschaftlicher Vortrag", "Twitter-Feed", 
+                     "S/W-Manuskript", "Wissenschaftliches Poster"),1)
+            ))
