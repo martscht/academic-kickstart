f61bbeb9a9785313845ca7b60ea37e6a77f20eb3
Martin Schultze <schultze@psych.uni-frankfurt.de>
2023-02-27 17:23:23 +0100

    Add KiJu Prep-Script.

---
+++ content/post/Preprocessing/KiJu_Preparation.R
@@ -0,0 +1,8 @@
+#### Vorbereitende Schritte zum KiJu Workshop ----
+install.packages('lme4')
+install.packages('lmerTest')
+install.packages('MuMIn')
+install.packages('jtools')
+install.packages('sjPlot')
+install.packages('ggplot2')
+install.packages('glmmTMB')
